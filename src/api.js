c2ec77d3e7f561bc2fbb653bb5c2c13eefe2f52d
raisavar <raisavar@users.noreply.github.com>
2025-06-24 11:22:21 +0300

    ci(api): fix null pointer exception

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 54
 module.exports = API;
+
+
+// API module for FiestaAPI
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 55
+module.exports = API;
