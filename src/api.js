ed7956cf7c4f358c6db9a1ff54bae507277376e3
raisavar <raisavar@users.noreply.github.com>
2025-11-05 02:01:30 +0200

    ci: fix memory leak

---
+++ src/api.js
@@ -0,0 +1,25 @@
+// API module for FiestaAPI
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 18
+module.exports = API;
